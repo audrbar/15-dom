31ddb5876523be85c1c665e8dcc5e5591322dd09
audrbar <102819309+audrbar@users.noreply.github.com>
2023-01-16 22:00:35 +0200

    event-listeners

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 console.clear();
 const h1DOM = document.getElementsByTagName('h1')[0];
-
 const text = h1DOM.textContent;
+console.log(h1DOM);
+console.log(text);
 
 const newText = text.trim().replace('rytas', 'diena');
-h1DOM.innerText = newText;
+console.log(newText);
 
-const colors = ['green', 'yellow'];
+const colors = ['green', 'yellow', 'red'];
 
 for (const color of colors) {
     h1DOM.classList.add(color);
@@ -15,16 +16,31 @@ for (const color of colors) {
 h1DOM.classList.remove('yellow');
 h1DOM.classList.toggle('red');
 
+console.log(h1DOM.classList);
+
+h1DOM.classList.add('green');
+h1DOM.classList.add('red');
 const arTuri1 = h1DOM.classList.contains('cipolinas');
 console.log(arTuri1);
 
 const arTuri2 = h1DOM.classList.contains('green');
 console.log(arTuri2);
 
+const buttonDOM = document.querySelector('button');
+// buttonDOM.addEventListener('click', labas);
+// function labas() {
+//     console.log('labas...');
+// }
+// console.log(buttonDOM);
+
+buttonDOM.addEventListener('click', () => {
+    console.log('labas...');
+});
+
 let score = 0;
 
 const scoreDOM = document.querySelector('.score');
-const [minusDOM, plusDOM] = document.querySelectorAll('button');
+const [clickMe, minusDOM, plusDOM] = document.querySelectorAll('button');
 
 minusDOM.addEventListener('click', () => {
     scoreDOM.innerText = --score;
